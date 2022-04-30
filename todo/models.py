777b69e71b2dd33a02145d5a32e051b93b0b3b60
Marudogir <illiahudilin@gmail.com>
2022-04-30 10:55:40 +0300

    Initial commit

---
+++ todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Todo(models.Model):
+    title = models.CharField(max_length=150)
+    memo = models.TextField(blank=True)
+    #установить текушее время
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.TextField(blank=True)
+    #пользователь что разместил
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
